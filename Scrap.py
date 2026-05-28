f3b17d00ae9c876173dc65d5f7e2baed9ca554a9
EthanKesler56 <ethan.kesler@protonmail.com>
2026-05-28 08:04:23 -0400

    PythonAbstractionPracticeDeckOfCards

--- Scrap.py
+++ Scrap.py
@@ -18,20 +18,35 @@ class DeckOfCards:
         "Queen",
         "King",
     ]
+    suit_order = {"Hearts":1,"Diamonds":2, "Clubs":3, "Spades":4}
 
     def __init__(self):
         self.__cards = []
         self.create_deck()
 
     def create_deck(self):
-        for suit in self.SUITS:
-            print(suit)
+        for rank in self.RANKS:
+            for suit in self.SUITS:
+                print(suit)
+                combo = (rank,suit)
+                
+                self.__cards.append(combo)
+                self.__cards.sort(key=lambda x: self.suit_order[x[1]])
+        print(self.__cards)
+                
 
     def shuffle_deck(self):
-        pass
+         random.shuffle(self.__cards)
 
     def deal_card(self):
-        pass
+        if self.__cards > 0:
+            return self.__cards.pop()
+        else: 
+            return None
+
+        
+        
+
 
     # don't touch below this line
 
